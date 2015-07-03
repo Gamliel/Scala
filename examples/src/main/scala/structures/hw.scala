ff60c74e613a7a4bad178f0762a0ae2003e877b8
DevTopic <pasquale@somewhere.com>
2015-07-03 11:21:26 +0100

    List example

--- examples/src/main/scala/structures/hw.scala
+++ examples/src/main/scala/structures/hw.scala
@@ -8,4 +8,14 @@ object Hi {
   def getGreetings(): String = {
     "Hi!"
   }
- }
+  
+  def getList(inf:Integer, sup:Integer): List[Integer] = {
+    if (sup < inf)
+      throw new IllegalArgumentException("Sup must be >= Inf")
+
+    if (inf == sup) List(inf)
+    else {
+      inf:: getList(inf+1,sup)
+    }
+  }
+}
--- examples/src/test/scala/structures/functionTests.scala
+++ examples/src/test/scala/structures/functionTests.scala
@@ -9,4 +9,8 @@ class functionTests extends FlatSpec{
   "A function" should "return 'Hi!'" in {
     assert( Hi.getGreetings() === "Hi!")
   }
+  
+  "A function" should "return a list with 10 elements" in{
+    assert ( Hi.getList(3,7) === List(3,4,5,6,7))
+  } 
 }
