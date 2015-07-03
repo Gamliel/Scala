08f214bf30d7c3f315e4014096ee3c0f4d296f7a
DevTopic <pasquale@somewhere.com>
2015-07-03 11:35:37 +0100

    Tail Optimisation

--- examples/src/main/scala/structures/hw.scala
+++ examples/src/main/scala/structures/hw.scala
@@ -15,7 +15,17 @@ object Hi {
 
     if (inf == sup) List(inf)
     else {
-      inf:: getList(inf+1,sup)
+      inf :: getList(inf+1,sup)
+    }
+  }
+
+  def getListOptimised(inf:Integer, sup:Integer, curList:List[Integer]): List[Integer] = {
+    if (sup < inf)
+      throw new IllegalArgumentException("Sup must be >= Inf")
+
+    if (inf == sup) inf::curList
+    else {
+      getListOptimised(inf+1, sup, inf::curList)
     }
   }
 }
--- examples/src/test/scala/structures/functionTests.scala
+++ examples/src/test/scala/structures/functionTests.scala
@@ -10,7 +10,18 @@ class functionTests extends FlatSpec{
     assert( Hi.getGreetings() === "Hi!")
   }
   
-  "A function" should "return a list with 10 elements" in{
+  "A list function" should "return a list with a range" in{
     assert ( Hi.getList(3,7) === List(3,4,5,6,7))
   } 
+
+  "A list function" should "fail generate 100000 elements" in{
+    intercept[java.lang.StackOverflowError] {
+      Hi.getList(1,100000)
+    }
+  } 
+
+  "An Optimised list function" should "generate 100000 elements" in{
+    val elements = Hi.getListOptimised(1,100000,List.empty)
+    assert ( elements.length  === 100000)
+  }
 }
